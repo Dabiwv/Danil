4603c3372960c60131bc73266a06d64d4af6059e
Dabiwv <anzelikadenile612@gmail.com>
2024-09-03 22:40:55 +0500

    Bot.py

--- Bot.py
+++ Bot.py
@@ -5,23 +5,18 @@ from telebot import types
 TOKEN = '7389514448:AAEAHWWJP6E6zisO0BqEoGrYD-DrRrWEEbs'
 bot = telebot.TeleBot(TOKEN)
 
-# Создание кнопок
-def start_buttons():
+# Функции для создания кнопок
+def main_menu():
     markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    item1 = types.KeyboardButton("Старт")
-    markup.add(item1)
-    return markup
-
-def admin_buttons():
-    markup = types.InlineKeyboardMarkup()
-    button = types.InlineKeyboardButton("Связь с админом", callback_data='contact_admin')
-    markup.add(button)
+    item1 = types.KeyboardButton("Услуги")
+    item2 = types.KeyboardButton("Связь с админом")
+    markup.add(item1, item2)
     return markup
 
 def services_buttons():
     markup = types.InlineKeyboardMarkup()
-    button1 = types.InlineKeyboardButton("Мануал от Sweet Roxie: Анонимность в интернете + бесплатный слитый VPN - 330 руб", callback_data='service_1')
-    button2 = types.InlineKeyboardButton("Софты для Termux/Linux (в наличии 12 штук) - 660 руб", callback_data='service_2')
+    button1 = types.InlineKeyboardButton("Анонимность в интернете + VPN - 330 руб", callback_data='service_1')
+    button2 = types.InlineKeyboardButton("Софты для Termux/Linux (12 штук) - 660 руб", callback_data='service_2')
     button3 = types.InlineKeyboardButton("Обучение OSINT Framework - 250 руб", callback_data='service_3')
     markup.add(button1, button2, button3)
     return markup
@@ -29,25 +24,21 @@ def services_buttons():
 # Обработка команды /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    markup = start_buttons()
-    bot.send_message(message.chat.id, "Пройдите текстовую капчу:", reply_markup=markup)
+    bot.send_message(message.chat.id, "Пройдите текстовую капчу для подтверждения.", reply_markup=main_menu())
 
-# Обработка текстовой капчи
-@bot.message_handler(func=lambda message: message.text == "Старт")
-def handle_start(message):
-    # Здесь вы можете добавить логику для проверки капчи
-    # Если проверка прошла успешно
-    bot.send_message(message.chat.id, "Проверка на человека удалась 🪄, для дальнейшего действия, выберите раздел", reply_markup=services_buttons())
-    # Если проверка не удалась
-    # bot.send_message(message.chat.id, "Проверка не удалась. Пожалуйста, попробуйте снова.")
+# Обработка нажатий на кнопки
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    if message.text == "Услуги":
+        bot.send_message(message.chat.id, "Выберите услугу:", reply_markup=services_buttons())
+    elif message.text == "Связь с админом":
+        bot.send_message(message.chat.id, "Напишите пожалуйста сюда для дальнейших действий @doksformoney")
 
-# Обработка нажатий на кнопки инлайн
+# Обработка нажатий на инлайн кнопки
 @bot.callback_query_handler(func=lambda call: True)
 def handle_query(call):
-    if call.data == 'contact_admin':
-        bot.answer_callback_query(call.id, "Напишите пожалуйста сюда для дальнейших действий @doksformoney")
-    elif call.data in ['service_1', 'service_2', 'service_3']:
-        bot.answer_callback_query(call.id, "Оплатить можно на карту РОССИИ: 2200701089399395 Амир, после оплаты надо связаться с автором данного бота для дальнейших переговоров")
+    if call.data == 'service_1' or call.data == 'service_2' or call.data == 'service_3':
+        bot.send_message(call.message.chat.id, "Оплатить можно на карту РОССИИ: 2200701089399395 Амир, после оплаты надо связаться с автором данного бота @doksformoney для дальнейших переговоров")
 
 # Запуск бота
 bot.polling()
