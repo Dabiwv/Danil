4a82fb810347d0cec1a54f9b6209f5e45705a274
Dabiwv <anzelikadenile612@gmail.com>
2024-09-05 08:32:15 +0500

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -6,20 +6,46 @@ bot = telebot.TeleBot("6699047318:AAGRIzgJy2LuPJWW59O0QsiuDCfZ20xxHws")
 
 # Словарь с аккаунтами
 accounts = {
-    "6 уровень": {"price": 200, "description": "Аккаунт 6 уровня. Простой набор вещей, небольшой дом и автомобиль."},
-    "10 уровень": {"price": 300, "description": "Аккаунт 10 уровня. Средний набор вещей и инвентаря, есть дом и хорошая машина."},
-    "15 уровень": {"price": 400, "description": "Аккаунт 15 уровня. Богатый набор вещей, большой дом, дорогая машина и множество аксессуаров."},
-    "20 уровень": {"price": 500, "description": "Аккаунт 20 уровня. Эксклюзивный набор вещей, роскошный особняк, самая дорогая машина, уникальные аксессуары."},
-    "23 уровень": {"price": 550, "description": "Аккаунт 23 уровня. Аккаунт миллиардера. Все самое лучшее: одежда, имущество, транспорт и аксессуары."}
-}
-
-# Словарь с кейсами
-cases = {
-    "Бомж-кейс": 140,
-    "Ежедневный кейс": 230,
-    "Стандартный кейс": 350,
-    "Особый кейс": 500,
-    "Кейс с Блэк Коинами": 700
+    "Начинающий аккаунт": {
+        "description": "Этот аккаунт подходит для начинающих игроков. В нем уже есть 23 бойца с максимальным рангом 10, а также 3000 кубков. Отлично подойдет для изучения основ игры.",
+        "trophies": 3000,
+        "brawlers": 23,
+        "brawler_ranks": 10,
+        "brawl_pass": False,
+        "price": 160
+    },
+    "Средний аккаунт": {
+        "description": "Средний аккаунт имеет 45 бойцов с рангом до 18, а также 12000 кубков. Этот аккаунт уже прошел этап освоения и готов для более интенсивной игры. Также включает активный Brawl Pass.",
+        "trophies": 12000,
+        "brawlers": 45,
+        "brawler_ranks": 18,
+        "brawl_pass": True,
+        "price": 320
+    },
+    "Продвинутый аккаунт": {
+        "description": "Продвинутый аккаунт обладает 55 бойцами с максимальным рангом 25 и 18000 кубков. Это отличный аккаунт для игроков, достигших высокого уровня мастерства. Также включает активный Brawl Pass.",
+        "trophies": 18000,
+        "brawlers": 55,
+        "brawler_ranks": 25,
+        "brawl_pass": True,
+        "price": 440
+    },
+    "Элитный аккаунт": {
+        "description": "Элитный аккаунт является одним из лучших. В нем 65 бойцов с рангом до 30 и 25000 кубков. Отлично подходит для соревновательной игры на высоком уровне. Также включает активный Brawl Pass.",
+        "trophies": 25000,
+        "brawlers": 65,
+        "brawler_ranks": 30,
+        "brawl_pass": True,
+        "price": 550
+    },
+    "Топ-аккаунт": {
+        "description": "Топ-аккаунт является наивысшим уровнем. В нем 78 бойцов с максимальным рангом 35 и 35000 кубков. Это идеальный выбор для опытных игроков, стремящихся к вершинам рейтинга. Также включает активный Brawl Pass.",
+        "trophies": 35000,
+        "brawlers": 78,
+        "brawler_ranks": 35,
+        "brawl_pass": True,
+        "price": 660
+    }
 }
 
 # Обработка команды /start
@@ -27,9 +53,8 @@ cases = {
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton("Аккаунты")
-    item2 = types.KeyboardButton("Кейсы")
-    markup.add(item1, item2)
-    bot.send_message(chat_id=message.chat.id, text="Привет! Что вы хотите купить?", reply_markup=markup)
+    markup.add(item1)
+    bot.send_message(chat_id=message.chat.id, text="Добро пожаловать в магазин аккаунтов Brawl Stars! Что вы хотите купить?", reply_markup=markup)
 
 # Обработка нажатия на кнопку "Аккаунты"
 @bot.message_handler(content_types=['text'])
@@ -37,33 +62,22 @@ def handle_text(message):
     if message.text == "Аккаунты":
         markup = types.InlineKeyboardMarkup()
         for account, data in accounts.items():
-            button = types.InlineKeyboardButton(text=account, callback_data=account)
+            button = types.InlineKeyboardButton(text=f"{account} - {data['price']} руб.", callback_data=account)
             markup.add(button)
         bot.send_message(chat_id=message.chat.id, text="Выберите аккаунт:", reply_markup=markup)
-
-    # Обработка нажатия на кнопку "Кейсы"
-    elif message.text == "Кейсы":
-        markup = types.InlineKeyboardMarkup()
-        for case, price in cases.items():
-            button = types.InlineKeyboardButton(text=f"{case} - {price} руб.", callback_data=case)
-            markup.add(button)
-        bot.send_message(chat_id=message.chat.id, text="Выберите кейс:", reply_markup=markup)
 
 # Обработка выбора аккаунта
 @bot.callback_query_handler(func=lambda call: call.data in accounts)
 def handle_account_selection(call):
     account = call.data
-    description = accounts[account]["description"]
-    price = accounts[account]["price"]
-    bot.send_message(chat_id=call.message.chat.id, text=f"Вы выбрали аккаунт {account}:\n\n{description}\n\nЦена: {price} рублей")
-    send_payment_instructions(call.message.chat.id)
-
-# Обработка выбора кейса
-@bot.callback_query_handler(func=lambda call: call.data in cases)
-def handle_case_selection(call):
-    case = call.data
-    price = cases[case]
-    bot.send_message(chat_id=call.message.chat.id, text=f"Вы выбрали {case} за {price} рублей")
+    data = accounts[account]
+    description = f"Выбран аккаунт: {account}\n\n{data['description']}\n\n" \
+                  f"Кубков: {data['trophies']}\n" \
+                  f"Бойцов: {data['brawlers']}\n" \
+                  f"Ранг бойцов: до {data['brawler_ranks']}\n" \
+                  f"Brawl Pass: {'Есть' if data['brawl_pass'] else 'Нет'}\n\n" \
+                  f"Цена: {data['price']} рублей"
+    bot.send_message(chat_id=call.message.chat.id, text=description)
     send_payment_instructions(call.message.chat.id)
 
 # Функция для отправки инструкций по оплате
@@ -79,12 +93,11 @@ def send_payment_instructions(chat_id):
 👨‍💻 Имя - Данил Г.
 💬 Комментарий: НЕ ПИСАТЬ!!!
 
-СБП - 
+СБП -
 Оплатить можно на карту РОССИИ: 2200701089399395 Аким.
 После оплаты свяжитесь с автором данного бота @doksformoney для дальнейших переговоров
 """
     markup = types.InlineKeyboardMarkup()
-
     telegram_button = types.InlineKeyboardButton(text="Оплатить через Telegram", url="tg://wallet?recipient=UQC8Y2ZLGUJSmAasHTw_VNvO5jQ4w4OeJC_DQBO-wnqUItAL")
     kaspi_button = types.InlineKeyboardButton(text="Оплатить через Kaspi Bank", url="https://kaspi.kz/u/4400430269346638")
     sbp_button = types.InlineKeyboardButton(text="Оплатить через СБП", url="https://sberbank.ru/ru/person/bank_cards/sbp")
